7ebb3debd062ca8d501255c4fc94a54d703af144
bill-95 <lin.qi2@mail.mcgill.ca>
2024-09-25 17:50:07 -0400

    Create UnspannedMacroRisks.m

---
+++ _publications/UnspannedMacroRisks.m
@@ -0,0 +1,18 @@
+---
+title: "Global Evidence on Unspanned Macro Risks in Dynamic Term Structure Model"
+authors:
+  - name: "Michel van der Wel"
+    url: "https://personal.eur.nl/vanderwel/"
+abstract: 'There are mixed results on whether macro risks are spanned by the yield curve. This paper reviews the major arguments and takes a global perspective to obtain comprehensive evidence. We study a large cross-section of 22 countries, including both developed and emerging markets. Our regression evidence confirms that macro information provides explanatory power for bond excess returns on top of yield factors, in fact more so for emerging than for developed countries. This finding is particularly strong in emerging markets. From a mechanical perspective, discriminating between spanned and unspanned models when considering in-sample fit and term premium predictions makes no difference, while Sharpe ratios are more realistic for unspanned models.'
+collection: publications
+category: working_papers
+permalink: /research/UnspannedMacroRisks
+date: 2021-05-28
+venue: 'Erasmus University Rotterdam, EEA 2020, CICF 2021, EcoMod 2021, SoFiE 2022*'
+paperurl: 'https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3853648'
+citation: 'van der Wel, Michel and Zhang, Yaoyuan, Global Evidence on Unspanned Macro Risks in Dynamic Term Structure Models (May 20, 2022). Available at SSRN: https://ssrn.com/abstract=3853648 or http://dx.doi.org/10.2139/ssrn.3853648'
+show_venue: true
+show_date: true
+---
+
+There are mixed results on whether macro risks are spanned by the yield curve. This paper reviews the major arguments and takes a global perspective to obtain comprehensive evidence. We study a large cross-section of 22 countries, including both developed and emerging markets. Our regression evidence confirms that macro information provides explanatory power for bond excess returns on top of yield factors, in fact more so for emerging than for developed countries. This finding is particularly strong in emerging markets. From a mechanical perspective, discriminating between spanned and unspanned models when considering in-sample fit and term premium predictions makes no difference, while Sharpe ratios are more realistic for unspanned models.
